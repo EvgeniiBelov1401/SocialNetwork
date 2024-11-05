b4c2d26909d4005eb86c441aa9577daeb800a6a8
Евгений <evgeniibelov1401@gmail.com>
2024-11-05 12:25:36 +0300

    Добавил подключение к БД

---
+++ Models/SocNetContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Models.Db;
+
+namespace SocialNetwork.Models
+{
+    public sealed class SocNetContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserPost> UserPosts { get; set; }
+
+
+        public SocNetContext(DbContextOptions<SocNetContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
