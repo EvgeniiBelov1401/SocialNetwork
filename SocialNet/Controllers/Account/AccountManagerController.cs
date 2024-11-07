d693f79c4c615be4c43c0610624d91c9a405b06c
Евгений <evgeniibelov1401@gmail.com>
2024-11-07 13:33:26 +0300

    hh

---
+++ SocialNet/Controllers/Account/AccountManagerController.cs
@@ -0,0 +1,344 @@
+﻿using AutoMapper;
+using SocialNet.Data;
+using SocialNet.Data.Repository;
+using SocialNet.Data.UoW;
+using SocialNet.Extentions;
+using SocialNet.Models.Users;
+using SocialNet.ViewModels.Account;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SocialNet.Controllers.Account
+{
+    public class AccountManagerController : Controller
+    {
+        private IMapper _mapper;
+
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        private IUnitOfWork _unitOfWork;
+
+        public AccountManagerController(UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+
+        }
+
+
+        [Route("Generate")]
+        [HttpGet]
+        public async Task<IActionResult> Generate()
+        {
+
+            var usergen = new GenetateUsers();
+            var userlist = usergen.Populate(35);
+
+            foreach(var user in userlist)
+            {
+                var result = await _userManager.CreateAsync(user, "123456");
+
+                if (!result.Succeeded)
+                    continue;
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Route("Login")]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+            // return View(new LoginViewModel());
+        }
+
+        [Authorize]
+        [Route("MyPage")]
+        [HttpGet]
+        public async Task<IActionResult> MyPage()
+        {
+            var user = User;
+
+            var result = await _userManager.GetUserAsync(user);
+
+            var model = new UserViewModel(result);
+
+            model.Friends = await GetAllFriend(model.User);
+
+            return View("User", model);
+        }
+
+        private async Task<List<User>> GetAllFriend(User user)
+        {
+            var repository = _unitOfWork.GetRepository<Friend>() as FriendsRepository;
+
+            return repository.GetFriendsByUser(user);
+        }
+
+        private async Task<List<User>> GetAllFriend()
+        {
+            var user = User;
+
+            var result = await _userManager.GetUserAsync(user);
+
+            var repository = _unitOfWork.GetRepository<Friend>() as FriendsRepository;
+
+            return repository.GetFriendsByUser(result);
+        }
+
+        [Route("Edit")]
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            var user = User;
+
+            var result = _userManager.GetUserAsync(user);
+
+            var editmodel = _mapper.Map<UserEditViewModel>(result.Result);
+
+            return View("Edit", editmodel);
+        }
+
+        [Authorize]
+        [Route("Update")]
+        [HttpPost]
+        public async Task<IActionResult> Update(UserEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.UserId);
+
+                user.Convert(model);
+
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("MyPage", "AccountManager");
+                }
+                else
+                {
+                    return RedirectToAction("Edit", "AccountManager");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Некорректные данные");
+                return View("Edit", model);
+            }
+        }
+        
+        public async Task<IActionResult> DeleteUser()
+        {
+            var user = User;
+
+            var result = await _userManager.GetUserAsync(user);
+            
+            var friendsRepository = _unitOfWork.GetRepository<Friend>() as FriendsRepository;
+            var messagesRepository = _unitOfWork.GetRepository<Message>() as MessageRepository;
+            await _signInManager.SignOutAsync();
+            
+            friendsRepository.ClearFriendsAsync(result);
+            messagesRepository.ClearMessagesAsync(result);
+            
+            await _userManager.DeleteAsync(result);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Route("Login")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                // var user = _mapper.Map<User>(model);
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                var result = await _signInManager.PasswordSignInAsync(user.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                   return RedirectToAction("MyPage", "AccountManager");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Route("Logout")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Route("UserList")]
+        [HttpGet]
+        public async Task<IActionResult> UserList(string search)
+        {
+            var currentuser = User;
+            var result = await _userManager.GetUserAsync(currentuser);
+            if (result == null)
+            {
+                return RedirectToAction("Login", "AccountManager");
+            }
+            else
+            {
+                var model = await CreateSearch(search);
+                return View("UserList", model);
+            }
+        }
+
+        [Route("AddFriend")]
+        [HttpPost]
+        public async Task<IActionResult> AddFriend(string id)
+        {
+            var currentuser = User;
+
+            var result = await _userManager.GetUserAsync(currentuser);
+
+            var friend = await _userManager.FindByIdAsync(id);
+
+            var repository = _unitOfWork.GetRepository<Friend>() as FriendsRepository;
+
+            await repository.AddFriendAsync(result, friend);
+
+
+            return RedirectToAction("MyPage", "AccountManager");
+        }
+
+
+        [Route("DeleteFriend")]
+        [HttpPost]
+        public async Task<IActionResult> DeleteFriend(string id)
+        {
+            var currentuser = User;
+
+            var result = await _userManager.GetUserAsync(currentuser);
+
+            var friend = await _userManager.FindByIdAsync(id);
+
+            var repository = _unitOfWork.GetRepository<Friend>() as FriendsRepository;
+
+            await repository.DeleteFriendAsync(result, friend);
+
+            return RedirectToAction("MyPage", "AccountManager");
+
+        }
+
+
+        private async Task<SearchViewModel> CreateSearch(string search)
+        {
+            var currentuser = User;
+
+            var result = await _userManager.GetUserAsync(currentuser);
+            var list = _userManager.Users.AsEnumerable().Where(x => x.Id != result.Id).ToList();
+            if (!string.IsNullOrEmpty(search))
+            {
+                list = _userManager.Users.AsEnumerable()
+                    .Where(x => x.Id != result.Id)
+                    .Where(x => x.GetFullName().ToLower().Contains(search.ToLower())).ToList();
+            }
+            
+            var withfriend = await GetAllFriend();
+
+            var data = new List<UserWithFriendExt>();
+            list.ForEach(x =>
+            {
+                var t = _mapper.Map<UserWithFriendExt>(x);
+                t.IsFriendWithCurrent = withfriend.Where(y => y.Id == x.Id || x.Id == result.Id).Count() != 0;
+                data.Add(t);
+            });
+
+            var model = new SearchViewModel()
+            {
+                UserList = data
+            };
+
+            return model;
+        }
+
+
+        [Route("Chat")]
+        [HttpPost]
+        public async Task<IActionResult> Chat(string id)
+        {
+            var model = await GenerateChat(id);
+            return View("Chat", model);
+        }
+
+        private async Task<ChatViewModel> GenerateChat(string id)
+        {
+            var currentuser = User;
+
+            var result = await _userManager.GetUserAsync(currentuser);
+            var friend = await _userManager.FindByIdAsync(id);
+
+
+            var repository = _unitOfWork.GetRepository<Message>() as MessageRepository;
+
+            var mess = repository.GetMessages(result, friend);
+
+            var model = new ChatViewModel()
+            {
+                You = result,
+                ToWhom = friend,
+                History = mess.OrderBy(x => x.Id).ToList(),
+            };
+
+            return model;
+        }
+
+        [Route("Chat")]
+        [HttpGet]
+        public async Task<IActionResult> Chat()
+        {
+
+            var id = Request.Query["id"];
+
+            var model = await GenerateChat(id);
+            return View("Chat", model);
+        }
+
+        [Route("NewMessage")]
+        [HttpPost]
+        public async Task<IActionResult> NewMessage(string id, ChatViewModel chat)
+        {
+            var currentuser = User;
+
+            var result = await _userManager.GetUserAsync(currentuser);
+            var friend = await _userManager.FindByIdAsync(id);
+
+            var repository = _unitOfWork.GetRepository<Message>() as MessageRepository;
+
+            var item = new Message()
+            {
+                Sender = result,
+                Recipient = friend,
+                Text = chat.NewMessage.Text,
+            };
+            await repository.Create(item);
+            ModelState.Clear();
+            var model = await GenerateChat(id);
+            return View("Chat", model);
+        }
+    }
+}
---
+++ SocialNet/Data/Repository/FriendsRepository.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialNet.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace SocialNet.Data.Repository
+{
+    public class FriendsRepository : Repository<Friend>
+    {
+        public FriendsRepository(ApplicationDbContext db) : base(db)
+        {
+            
+        }
+        
+
+        public async Task AddFriendAsync (User target, User friend)
+        {
+           var friends = await Set.FirstOrDefaultAsync(x => x.UserId == target.Id && x.CurrentFriendId == friend.Id);
+        
+            if (friends == null)
+            {
+                var item = new Friend()
+                {
+                    UserId = target.Id,
+                    User = target,
+                    CurrentFriend = friend,
+                    CurrentFriendId = friend.Id,
+                };
+        
+                await Create(item);
+            }
+        }
+        // public void AddFriend (User target, User Friend)
+        // {
+        //     var friends = Set.AsEnumerable().FirstOrDefault(x => x.UserId == target.Id && x.CurrentFriendId == Friend.Id);
+        //
+        //     if (friends == null)
+        //     {
+        //         var item = new Friend()
+        //         {
+        //             UserId = target.Id,
+        //             User = target,
+        //             CurrentFriend = Friend,
+        //             CurrentFriendId = Friend.Id,
+        //         };
+        //
+        //         Create(item);
+        //     }
+        // }
+
+        public List<User> GetFriendsByUser(User target)
+        {
+            var userFriends = Set.AsEnumerable().FirstOrDefault(x => x.UserId == target.Id);
+            if (userFriends != null)
+            {
+                var friends = Set.Include(x => x.CurrentFriend).AsEnumerable().Where(x => x.UserId == target.Id).Select(x => x.CurrentFriend);
+                return friends.ToList();
+            }
+
+            return new List<User>();
+        }
+
+
+        // public void DeleteFriend(User target, User friend)
+        // {
+        //     var friends = Set.AsEnumerable().FirstOrDefault(x => x.UserId == target.Id && x.CurrentFriendId == friend.Id);
+        //
+        //     if (friends != null)
+        //     {
+        //         Delete(friends);
+        //     }
+        // }
+        public async Task DeleteFriendAsync(User target, User friend)
+        {
+            var friends = await Set.FirstOrDefaultAsync(x => x.UserId == target.Id && x.CurrentFriendId == friend.Id);
+
+            if (friends != null)
+            {
+                await Delete(friends);
+            }
+        }
+
+        public async Task ClearFriendsAsync(User user)
+        {
+            var friends = Set.AsEnumerable().Where(x => x.UserId == user.Id || x.CurrentFriendId == user.Id).ToList();
+            foreach (var friend in friends)
+            {
+                await Delete(friend);
+            }
+        }
+
+    }
+}
