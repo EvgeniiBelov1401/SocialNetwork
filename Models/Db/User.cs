ece031ca5103f84a40bcff952dcb9d41ec8386e5
Евгений <evgeniibelov1401@gmail.com>
2024-11-07 13:27:08 +0300

    ggg

--- Models/Db/User.cs
+++ Models/Db/User.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.IO;
 
 namespace SocialNetwork.Models.Db
 {
@@ -12,6 +13,25 @@ namespace SocialNetwork.Models.Db
         public string MiddleName { get; set; }
 
         public DateTime BirthDate { get; set; }
+
+        public string Image { get; set; }
+
+        public string Status { get; set; }
+
+        public string About { get; set; }
+
+        public string GetFullName()
+        {
+            return UserName + " " + MiddleName + " " + LastName;
+        }
+
+        public User()
+        {
+            Image = Path.GetFullPath(Path.Combine("..", "Static", "klipartz.com.png"));
+            Status = "Ура! Я в соцсети!";
+            About = "Информация обо мне.";
+
+        }
 
     }
 }
